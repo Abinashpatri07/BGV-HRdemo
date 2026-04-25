2b320f49d091d708401a37461242d6a4aaa38464
dqnex-abinash <abinash.patri@dqnex.com>
2026-04-25 12:39:12 +0530

    3nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashBoard from './pages/DashBoard'
 import Home from './pages/Home';
-import MainLayout from './layout/MainLayOut';
+import MainLayout from './Layout/MainLayout';
 import CreateCandidateForm from './components/CreateCandidateForm';
 
 
